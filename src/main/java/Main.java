0d7934c835d64aeb82f01fc17b123f4f1a474c63
I-Am-Morald <slogoff@yandex.ru>
2025-02-18 22:39:57 +0300

    Разработка первой проектной работы.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,57 @@
+import java.util.Scanner;
+
+class Car {
+    int speed;
+    String carName;
+    public Car(String carName, int speed) {
+        this.carName = carName;
+        this.speed = speed;
+    }
+}
+
+class Race {
+    String leaderName;
+    int distance;
+    public void takeLeader(Car car) {
+        int distance = car.speed * 24;
+        if (this.distance < distance) {
+            this.leaderName = car.carName;
+            this.distance = distance;
+        }
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String carName;
+        int speed;
+        Race race = new Race();
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Введите название автомобиля № " + i);
+            carName = scanner.next();
+            while (true) {
+                System.out.println("Введите скорость автомобиля № " + i);
+                if (scanner.hasNextInt()) {
+                    speed = scanner.nextInt();
+                    if (speed > 0 && speed <= 250) {
+                        break;
+                    }
+                    else {
+                        System.out.println("Пожалуйста, введите корректную скорость. Повторите ввод!");
+                    }
+                }
+                else {
+                    scanner.next();
+                    System.out.println("Пожалуйста, введите целое число. Повторите ввод!");
+                }
+
+            }
+            Car car = new Car(carName, speed);
+            race.takeLeader(car);
+        }
+        System.out.println("Лидер гонки - автомобиль '" + race.leaderName + "'");
     }
 }
+
+
